4956d92bfcf16f92370f1077e5bc71721debd4d5
luzcry <mcrodriguezt618@gmail.com>
2021-02-12 00:21:14 -0500

    adding query params

--- app/routes/pokemon.js
+++ app/routes/pokemon.js
@@ -1,4 +1,16 @@
 import Route from '@ember/routing/route';
 
 export default class PokemonRoute extends Route {
+  queryParams = {
+    offset: {
+      refreshModel: true,
+    },
+    model(params) {
+      const offset = params.offset ? params.offset : 0;
+
+      return this.store.queryRecord('pokemon', {
+        offset,
+      });
+    },
+  };
 }
